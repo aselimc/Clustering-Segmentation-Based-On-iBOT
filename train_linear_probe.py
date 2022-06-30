064e0a85c07a840f6ea57973ad3485b5aeab9436
OmegaMadPenguin <rahnfelj@informatik.uni-freiburg.de>
2022-06-30 13:01:03 +0200

    add wandb support

--- train_linear_probe.py
+++ train_linear_probe.py
@@ -1,8 +1,9 @@
 import argparse
+from datetime import datetime
 from random import shuffle
-from gpg import Data
 import torch
 import torch.nn as nn
+import wandb
 
 
 from utils import mIoU
@@ -16,6 +17,31 @@ from torch.utils.data import DataLoader
 import models
 from torch.utils.tensorboard import SummaryWriter
 
+
+CLASS_LABELS = {
+    0: "background",
+    1: "aeroplane",
+    2: "bicycle",
+    3: "bird",
+    4: "boat",
+    5: "bottle",
+    6: "bus",
+    7: "car",
+    8: "cat",
+    9: "chair",
+    10: "cow",
+    11: "diningtable",
+    12: "dog",
+    13: "horse",
+    14: "motorbike",
+    15: "person",
+    16: "pottedplant",
+    17: "sheep",
+    18: "sofa",
+    19: "train",
+    20: "tvmonitor",
+}
+
 global_step = 0
 
 def parser_args():
@@ -23,10 +49,10 @@ def parser_args():
     parser.add_argument('--root', default="data")
     parser.add_argument('--weights', default="weights/ViT-S16.pth")
     parser.add_argument('--arch', default="vit_small")
-    parser.add_argument('--patch_size', default=8)
+    parser.add_argument('--patch_size', default=16)
     parser.add_argument('--epochs', default=500)
     parser.add_argument('--n_blocks', default=4)
-    parser.add_argument('--batch_size', default=16)
+    parser.add_argument('--batch_size', default=1)
     parser.add_argument("--init_lr", default=1e-3)
     parser.add_argument("--min_lr", default=1e-5)
     parser.add_argument("--percentage", default=0.1)
@@ -57,15 +83,17 @@ def train(loader, backbone, classifier, criterion, optimizer, n_blocks, schedule
         loss_l.append(loss.item())
         progress_bar.update()
         global_step += 1
+
     return np.mean(np.array(loss_l))
 
 
-def validate(loader, backbone, classifier, criterion, n_blocks, logger):
+def validate(loader, backbone, classifier, criterion, n_blocks):
     backbone.eval()
     classifier.eval()
     val_loss = []
     miou_arr = []
-    random_pic_select = np.random.randint(len(loader))
+    #random_pic_select = np.random.randint(len(loader))
+    random_pic_select = 1
     with torch.no_grad():
         for idx , (img, segmentation) in enumerate(loader):
             img = img.cuda()
@@ -79,9 +107,24 @@ def validate(loader, backbone, classifier, criterion, n_blocks, logger):
             miou_arr.append(miou.item())
             if random_pic_select==idx:
                 print("Adding Image Example to Logger")
-                logger.add_image("gt_image", img[0], global_step)
-                logger.add_image("pred_image", torch.argmax(linear_output, dim=1), global_step)
-                logger.add_image("gt_segmentation", segmentation, global_step)
+                pred_segmentation = wandb.Image(img[0],
+                    masks={
+                        "predictions": {
+                            "mask_data": torch.argmax(linear_output, dim=1).squeeze(0).cpu().numpy(),
+                            "class_labels": CLASS_LABELS
+                            }
+                        })
+                gt_segmentation = wandb.Image(img[0],
+                    masks={
+                        "ground_truth": {
+                            "mask_data": segmentation.squeeze(0).cpu().numpy(),
+                            "class_labels": CLASS_LABELS
+                            }
+                        })
+                wandb.log({
+                    "Pred Segmentation": pred_segmentation,
+                    "GT Segmentation": gt_segmentation},
+                    step=global_step)
 
     return np.mean(np.array(miou_arr)), np.mean(np.array(val_loss))
 
@@ -101,8 +144,21 @@ class ConvLinearClassifier(nn.Module):
 
 
 def main(args):
+    config = {
+        "arch": args.arch,
+        "epochs": args.epochs,
+        "batch_size": args.batch_size,
+        "learning_rate": args.init_lr,
+        "patch_size": args.patch_size,
+        "number_blocks": args.n_blocks,
+        "percentage_train_labels": args.percentage,
+    }
     
-    logger = SummaryWriter(args.log_folder)
+    wandb.init(
+        project="iBot",
+        entity="dl_lab_enjoyers",
+        name=datetime.now().strftime('%m.%d.%Y-%H:%M:%S'),
+        config=config)
 
     # Loading the backbone
     backbone = models.__dict__[args.arch](
@@ -111,7 +167,7 @@ def main(args):
     )
 
     state_dict = torch.load(args.weights)['state_dict']
-    backbone.load_state_dict(state_dict)
+    #backbone.load_state_dict(state_dict)
     backbone = backbone.cuda()
     
         
@@ -151,12 +207,14 @@ def main(args):
     for epoch in range(args.epochs):
         mean_loss = train(train_loader, backbone, classifier, criterion, optimizer, n_blocks, lr_scheduler)
         print(f"For epoch number {epoch} --> Average Loss {mean_loss:.2f}")
-        logger.add_scalar("training_loss", mean_loss, global_step)
+        wandb.log({"training_loss": mean_loss}, step=global_step)
         if epoch % 10 == 0:
-            miou, loss = validate(val_loader, backbone, classifier, criterion, n_blocks, logger)
+            miou, loss = validate(val_loader, backbone, classifier, criterion, n_blocks)
             print(f"Validation for epoch {epoch}: Average mIoU {miou}, Average Loss {loss}")
-            logger.add_scalar("val_loss", loss, global_step)
-            logger.add_scalar("val_miou", miou, global_step)
+            wandb.log({
+                "val_loss": loss,
+                "val_miou": miou
+            })
         if epoch == 55:
             x = 3
 
