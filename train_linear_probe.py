dbfc1df4599baf772067a25df626c6cf44b4bf57
OmegaMadPenguin <rahnfelj@informatik.uni-freiburg.de>
2022-07-01 22:14:10 +0200

    adapt training settings to dino/ibot

--- train_linear_probe.py
+++ train_linear_probe.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from random import shuffle
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import wandb
 
 
@@ -40,28 +41,26 @@ CLASS_LABELS = {
     18: "sofa",
     19: "train",
     20: "tvmonitor",
+    255: "contours"
 }
 
 global_step = 0
 
 def parser_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--root', default="data")
-    parser.add_argument('--weights', default="weights/ViT-S16.pth")
-    parser.add_argument('--arch', default="vit_small")
-    parser.add_argument('--patch_size', default=16)
-    parser.add_argument('--epochs', default=500)
-    parser.add_argument('--n_blocks', default=4)
-    parser.add_argument('--batch_size', default=1)
-    parser.add_argument("--init_lr", default=1e-3)
-    parser.add_argument("--min_lr", default=1e-5)
-    parser.add_argument("--percentage", default=0.1)
-    parser.add_argument("--log_folder", default="logs/")
-    
+    parser.add_argument('--root', type=str, default="data")
+    parser.add_argument('--weights', type=str, default="weights/ViT-S16.pth")
+    parser.add_argument('--arch', type=str, default="vit_small")
+    parser.add_argument('--patch_size', type=int, default=16)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--n_blocks', type=int, default=4)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--percentage", type=float, default=0.1)
 
     return parser.parse_args()
 
-def train(loader, backbone, classifier, criterion, optimizer, n_blocks, scheduler):
+def train(loader, backbone, classifier, criterion, optimizer, n_blocks):
     global global_step
     backbone.eval()
     loss_l = []
@@ -74,12 +73,14 @@ def train(loader, backbone, classifier, criterion, optimizer, n_blocks, schedule
         with torch.no_grad():
             intermediate_output = backbone.get_intermediate_layers(img, n_blocks)
             output = torch.cat([x[:, 1:] for x in intermediate_output], dim=-1).detach()
-        linear_output= classifier(output)
+        pred_logits = classifier(output)
 
-        loss = criterion(linear_output, segmentation.long())
+        loss = criterion(pred_logits, segmentation.long())
+        loss[segmentation==255.0] = 0.0
+        loss = loss.sum() / loss.numel()
         loss.backward()
         optimizer.step()
-        scheduler.step()
+
         loss_l.append(loss.item())
         progress_bar.update()
         global_step += 1
@@ -93,37 +94,42 @@ def validate(loader, backbone, classifier, criterion, n_blocks):
     val_loss = []
     miou_arr = []
     random_pic_select = np.random.randint(len(loader))
-    with torch.no_grad():
-        for idx , (img, segmentation) in enumerate(loader):
-            img = img.cuda()
-            segmentation = segmentation.cuda()
+    for idx, (img, segmentation) in enumerate(loader):
+        img = img.cuda()
+        segmentation = segmentation.cuda()
+        with torch.no_grad():
             intermediate_output = backbone.get_intermediate_layers(img, n_blocks)
             output = torch.cat([x[:, 1:] for x in intermediate_output], dim=-1).detach()
-            linear_output = classifier(output)
-            loss = criterion(linear_output, segmentation.long())
-            val_loss.append(loss.item())
-            miou = mIoU(linear_output,segmentation)
-            miou_arr.append(miou.item())
-            if random_pic_select==idx:
-                print("Adding Image Example to Logger")
-                pred_segmentation = wandb.Image(img[0],
-                    masks={
-                        "predictions": {
-                            "mask_data": torch.argmax(linear_output, dim=1).squeeze(0).cpu().numpy(),
-                            "class_labels": CLASS_LABELS
-                            }
-                        })
-                gt_segmentation = wandb.Image(img[0],
-                    masks={
-                        "ground_truth": {
-                            "mask_data": segmentation.squeeze(0).cpu().numpy(),
-                            "class_labels": CLASS_LABELS
-                            }
-                        })
-                wandb.log({
-                    "Pred Segmentation": pred_segmentation,
-                    "GT Segmentation": gt_segmentation},
-                    step=global_step)
+        pred_logits = classifier(output)
+
+        loss = criterion(pred_logits, segmentation.long())
+        loss[segmentation==255.0] = 0.0
+        loss = loss.sum() / loss.numel()
+
+        val_loss.append(loss.item())
+        miou = mIoU(pred_logits, segmentation)
+        miou_arr.append(miou.item())
+
+        if random_pic_select==idx:
+            print("Adding Image Example to Logger")
+            pred_segmentation = wandb.Image(img[0],
+                masks={
+                    "predictions": {
+                        "mask_data": torch.argmax(pred_logits, dim=1).squeeze(0).cpu().numpy(),
+                        "class_labels": CLASS_LABELS
+                        }
+                    })
+            gt_segmentation = wandb.Image(img[0],
+                masks={
+                    "ground_truth": {
+                        "mask_data": segmentation.squeeze(0).cpu().numpy(),
+                        "class_labels": CLASS_LABELS
+                        }
+                    })
+            wandb.log({
+                "Pred Segmentation": pred_segmentation,
+                "GT Segmentation": gt_segmentation},
+                step=global_step)
 
     return np.mean(np.array(miou_arr)), np.mean(np.array(val_loss))
 
@@ -136,9 +142,11 @@ class ConvLinearClassifier(nn.Module):
     def forward(self,x):
         bs, h_sqrt , ch= x.shape
         h = int(np.sqrt(h_sqrt))
-        x =  x.view(bs,ch, h, h)
+        
+        x = x.contiguous().view(bs,ch, h, h)
         x = self.conv1(x)
         x = Fun.interpolate(x, size=[224, 224], mode="bilinear", align_corners=True)
+    
         return x
 
 
@@ -147,7 +155,7 @@ def main(args):
         "arch": args.arch,
         "epochs": args.epochs,
         "batch_size": args.batch_size,
-        "learning_rate": args.init_lr,
+        "learning_rate": args.lr,
         "patch_size": args.patch_size,
         "number_blocks": args.n_blocks,
         "percentage_train_labels": args.percentage,
@@ -173,38 +181,37 @@ def main(args):
     for param in backbone.parameters():
         param.requires_grad = False
 
-    train_transform = Compose([
-        RandomCrop(224),
-        PILToTensor(),
-        MergeContours(),
-        RandomHorizontalFlip(),
-        Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    ])
     n_blocks = args.n_blocks
     embed_dim = backbone.embed_dim * n_blocks
     classifier = ConvLinearClassifier(embed_dim, n_classes=21).cuda()
-    optimizer = torch.optim.AdamW(classifier.parameters(), lr=args.init_lr)
 
-    
-    criterion = nn.CrossEntropyLoss()
     ## TRAINING DATASET ##
+    train_transform = Compose([
+        RandomCrop(224),
+        PILToTensor(),
+        RandomHorizontalFlip(),
+        Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+
     train_dataset = PartialDatasetVOC(percentage = args.percentage, root=args.root, image_set='train', download=False, transforms=train_transform)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size)
-
-    ## TRAINING DATASET ##
     val_dataset = datasets.VOCSegmentation(root=args.root, image_set='val', download=False, transforms=train_transform)
     val_loader = DataLoader(val_dataset, batch_size=1)
-    ####################################################
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs, eta_min=1e-5)
-    ####################################################
 
-    for param in backbone.parameters():
-        param.requires_grad = False
+    optimizer = optim.SGD(
+        classifier.parameters(),
+        args.lr * args.batch_size / 256.0,
+        momentum=0.9,
+        weight_decay=0
+    )
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs, eta_min=0)
+
+    criterion = nn.CrossEntropyLoss(reduction='none')
 
     ############## TRAINING LOOP #######################
 
     for epoch in range(args.epochs):
-        mean_loss = train(train_loader, backbone, classifier, criterion, optimizer, n_blocks, lr_scheduler)
+        mean_loss = train(train_loader, backbone, classifier, criterion, optimizer, n_blocks)
         print(f"For epoch number {epoch} --> Average Loss {mean_loss:.2f}")
         wandb.log({"training_loss": mean_loss}, step=global_step)
         if epoch % 10 == 0:
@@ -214,8 +221,8 @@ def main(args):
                 "val_loss": loss,
                 "val_miou": miou
             })
-        if epoch == 55:
-            x = 3
+        
+        lr_scheduler.step()
 
 def cosine_scheduler(base_value, final_value, epochs, niter_per_ep, warmup_epochs=0, start_warmup_value=0):
     iters = np.arange(epochs * niter_per_ep)
