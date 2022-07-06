c7b2259ad2c67f965f3d4663c7f22a39c93052cc
OmegaMadPenguin <rahnfelj@informatik.uni-freiburg.de>
2022-07-06 13:21:38 +0200

    update mIoU

--- utils.py
+++ utils.py
@@ -890,17 +890,26 @@ def compute_map(ranks, gnd, kappas=[]):
 
     return map, aps, pr, prs
 
-def mIoU(logits, gt, threshold=0.5):
-    if logits.shape[1] == 1 or len(logits.shape)==3:
-        if threshold==0.5:
-            pred = logits.round().byte()
-        else:
-            pred = logits > threshold
-    else:
-        pred = logits.argmax(dim=1).byte()
-    intersection = ((pred == 1) & (gt == 1)).sum().float()
-    union = ((pred == 1) | (gt == 1)).sum().float()
-    return intersection/(union+1.)
+def mIoU(label, pred, num_classes=21):
+    pred = torch.softmax(pred.float(), dim=1)              
+    pred = torch.argmax(pred, dim=1).squeeze(1)
+    iou_list = list()
+    present_iou_list = list()
+
+    pred = pred.view(-1)
+    label = label.view(-1)
+    for sem_class in range(num_classes):
+        pred_inds = (pred == sem_class)
+        target_inds = (label == sem_class)
+        if target_inds.long().sum().item() == 0:
+            iou_now = float('nan')
+        else: 
+            intersection_now = (pred_inds[target_inds]).long().sum().item()
+            union_now = pred_inds.long().sum().item() + target_inds.long().sum().item() - intersection_now
+            iou_now = float(intersection_now) / float(union_now)
+            present_iou_list.append(iou_now)
+        iou_list.append(iou_now)
+    return np.mean(present_iou_list)
 
 
 class MaskedCrossEntropyLoss(nn.Module):
