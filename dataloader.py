79360bf312bda0006efec02546ed9aaa86e780b5
aselimc <asc.connected@gmail.com>
2022-07-19 12:37:52 +0200

    loader_fix

--- dataloader.py
+++ dataloader.py
@@ -5,6 +5,6 @@ class PartialDatasetVOC(datasets.VOCSegmentation):
     def __init__(self, percentage, root, year='2012', image_set='train', download=False, transform=None, target_transform=None, transforms=None):
         super().__init__(root, year, image_set, download, transform, target_transform, transforms)
         self.percentage = percentage
-        
-        self.images = self.images[:int(percentage*len(self.images))]
-        self.masks = self.masks[:int(percentage*len(self.masks))]
+        if percentage < 1:
+            self.images = self.images[:int(percentage*len(self.images))]
+            self.targets = self.targets[:int(percentage*len(self.targets))]
