e620114cd173c8d3bf113c311a1d22f2d6269bb9
OmegaMadPenguin <rahnfelj@informatik.uni-freiburg.de>
2022-07-26 13:37:40 +0200

    add simple script for eval_knn

---
+++ example/eval_knn.sh
@@ -0,0 +1,22 @@
+ROOT="/home/rahnfelj"
+WEIGHTS="/home/rahnfelj/ibot_weights/ImageNet-22K/ViT-L.pth"
+ARCH="vit_large"
+PATCH_SIZE="16"
+N_BLOCKS="1"
+FEATURES=("intermediate" "query" "key" "value")
+PERCENTAGE=("0.01" "0.1" "1.0")
+BACKGROUND_LABEL_PERCENTAGE="1.0"
+N_NEIGHBORS="20"
+BATCH_SIZE="16"
+NUM_WORKERS="4"
+
+for feature in ${FEATURES[*]}
+do
+    for percentage in ${PERCENTAGE[*]}
+    do
+        python eval_knn.py --root $ROOT --weights $WEIGHTS --arch $ARCH \
+        --patch_size $PATCH_SIZE --n_blocks $N_BLOCKS --feature $feature \
+        --percentage $percentage --background_label_percentage $BACKGROUND_LABEL_PERCENTAGE \
+        --n_neighbors $N_NEIGHBORS --batch_size $BATCH_SIZE --workers $NUM_WORKERS
+    done
+done
